634933142fd14c3b7732e9f0f5d1033afaa3636e
Sarah Thoorens <[s.thoorens@gmail.com]>
2022-12-04 21:51:08 -0600

    debugging

--- client/src-sw.js
+++ client/src-sw.js
@@ -4,8 +4,8 @@ const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
-
 precacheAndRoute(self.__WB_MANIFEST);
+
 
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
